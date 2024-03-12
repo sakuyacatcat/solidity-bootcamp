4f6fc0a76be9cf64a86002382f73b40d96092ef6
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-03-13 03:40:34 +0900

    homework2

--- contracts/src/homework2.sol
+++ contracts/src/homework2.sol
@@ -2,21 +2,12 @@
 pragma solidity ^0.8.19;
 
 contract Homework2 {
-    uint[] private numbers;
+    uint8[] public array;
 
-    constructor() {
-        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    }
-
-    function delItem(uint index) public {
-        require(index < numbers.length, "Index out of range");
-        for (uint i = index; i < numbers.length - 1; i++) {
-            numbers[i] = numbers[i + 1];
-        }
-        numbers.pop();
-    }
-
-    function getNumbers() public view returns (uint[] memory) {
-        return numbers;
+    function remove(uint8 index) public {
+        require(index < array.length, "Index out of bounds");
+        require(array.length > 0, "Array is empty");
+        array[index] = array[array.length - 1];
+        array.pop();
     }
 }
