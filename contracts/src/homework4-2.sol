05f56f4fa6f264e4ccd53ceea3b1107bc40ab5aa
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-03-15 23:05:49 +0900

    add

---
+++ contracts/src/homework4-2.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract MyCon {
+    function a(
+        function(
+            function(
+                function(
+                    function(
+                        function(
+                            function(
+                                function (
+                                    function(
+                                        function() external pure
+                                    ) external pure
+                                ) external pure
+                            ) external pure
+                        ) external pure
+                    ) external pure
+                ) external pure
+            ) external pure
+        ) external pure
+    ) external pure {}
+}
