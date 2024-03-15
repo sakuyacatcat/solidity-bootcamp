4652d09e3d6523ec9daafaedcb64519cf51af97e
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-03-15 22:49:19 +0900

    reordering storage variables

--- contracts/src/homework4.sol
+++ contracts/src/homework4.sol
@@ -5,22 +5,22 @@ contract Store {
 
     struct payments {
         bool valid;
-        uint256 amount;
+        bool checked;
         address sender;
         uint8 paymentType;
-        uint256 finalAmount;
         address receiver;
+        uint256 amount;
+        uint256 finalAmount;
         uint256 initialAmount;
-        bool checked;
     }
-    uint8 index;
-    uint256 public number;
     bool flag1;
-    address admin;
-    mapping (address=>uint256) balances;
     bool flag2;
-    address admin2;
     bool flag3;
+    uint8 index;
+    address admin;
+    address admin2;
+    uint256 public number;
+    mapping (address=>uint256) balances;
     payments[8] topPayments;
 
 
